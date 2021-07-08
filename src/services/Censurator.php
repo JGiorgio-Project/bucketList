9ff56ce6aa7a1a22356aad020a214d3f711b95bf
JGiorgio-Project <giorgio.j@hotmail.fr>
2021-07-08 18:25:34 +0200

    Ajout deploiement. TP module 10

--- src/services/Censurator.php
+++ src/services/Censurator.php
@@ -7,6 +7,12 @@ namespace App\services;
 class Censurator
 {
 
+    /**
+     * Fonction qui permet de verifier la présence de mots interdits
+     * dans une chaine de caractère et de les remplacer par des *****.
+     * @param String $text
+     * @return String
+     */
     public function purify(String $text):String{
         $search = ['toto', 'titi', 'tata'];
         $remplace = '******';
